e8eb22a8f52554b46adf07d0af1253cd572825d4
hungngo04 <ngotienhung195@gmail.com>
2022-11-30 10:23:06 -0500

    pages

--- booking/src/App.js
+++ booking/src/App.js
@@ -7,6 +7,8 @@ import {
   Routes
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List"
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         {/* route */}
         <Route path="/" element={<Home/>} />
+        <Route path="/hotels" element={<List/>} />
+        <Route path="/hotels/:id" element={<Hotel/>} />
       </Routes>
     </BrowserRouter>
   );
