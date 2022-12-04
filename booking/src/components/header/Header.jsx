5520f900e851caf02d4d0845b02f873841cd343b
hungngo04 <ngotienhung195@gmail.com>
2022-12-03 23:49:19 -0500

    font-awesome

--- booking/src/components/header/Header.jsx
+++ booking/src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { faBed } from "@fortawesome/free-solid-svg-icons" 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
+
 
 const Header = () => {
   return (
     <div className='header'>
         <div className="headerList">
             <div className="headerListItem">
-                
+            <FontAwesomeIcon icon={faBed} />
             </div>
         </div>
     </div>
