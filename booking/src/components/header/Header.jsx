39a0c4c4385a51a82344dd0138410316f7cb575d
hungngo04 <ngotienhung195@gmail.com>
2022-12-08 23:51:36 -0500

    add icon

--- booking/src/components/header/Header.jsx
+++ booking/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { faBed } from "@fortawesome/free-solid-svg-icons" 
+import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons" 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome" 
 
 
@@ -8,7 +8,24 @@ const Header = () => {
     <div className='header'>
         <div className="headerList">
             <div className="headerListItem">
-            <FontAwesomeIcon icon={faBed} />
+                <FontAwesomeIcon icon={faBed} />
+                <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Airport taxis</span>
             </div>
         </div>
     </div>
