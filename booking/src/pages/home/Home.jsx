91926391a42fb90030f1474e7800caff30b1f131
hungngo04 <ngotienhung195@gmail.com>
2022-12-16 00:09:35 -0500

    featured

--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import './home.css'
 
 const Home = () => {
   return (
