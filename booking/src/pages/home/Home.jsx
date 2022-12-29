55dcaa618cdfc6e75fb746f067112dd13025f246
hungngo04 <ngotienhung195@gmail.com>
2022-12-28 23:50:12 -0500

    properties

--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
 
 const Home = () => {
@@ -10,6 +11,8 @@ const Home = () => {
       <Header></Header>
       <div className="homeContainer">
         <Featured></Featured>
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList></PropertyList>
       </div>
     </div>
   )
