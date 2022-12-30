9b9ec8453b386a09a475aafaf4eca86c46cacbd0
hungngo04 <ngotienhung195@gmail.com>
2022-12-30 17:28:56 -0500

    footer

---
+++ booking/src/components/footer /Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className="fLists">
+            <li className="fListItem">Countries</li>
+            <li className="fListItem">Regions</li>
+            <li className="fListItem">Cities</li>
+            <li className="fListItem">Districts</li>
+            <li className="fListItem">Airports</li>
+            <li className="fListItem">Hotels</li>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
